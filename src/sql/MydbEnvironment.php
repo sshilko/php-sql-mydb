b6c70d2f43f9fec5d8564706cfb45398ed4cb0a9
Sergei Shilko <contact@sshilko.com>
2022-10-15 23:58:43 +0200

    finalize coding standard

--- src/sql/MydbEnvironment.php
+++ src/sql/MydbEnvironment.php
@@ -29,10 +29,12 @@ use const E_STRICT;
  * @package sshilko/php-sql-mydb
  * @see https://github.com/sshilko/php-sql-mydb
  *
- * @phpcs:disable PSR1.Methods.CamelCapsMethodName
  */
 class MydbEnvironment
 {
+    /**
+     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     */
     public function gc_collect_cycles(): void
     {
         if (!gc_enabled()) {
@@ -42,6 +44,9 @@ class MydbEnvironment
         gc_collect_cycles();
     }
 
+    /**
+     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     */
     public function set_error_handler(?callable $callback, int $error_levels = E_ALL|E_STRICT): ?callable
     {
         return set_error_handler($callback, $error_levels);
@@ -52,21 +57,33 @@ class MydbEnvironment
         return $this->ini_set('mysqlnd.net_read_timeout', $timeoutSeconds);
     }
 
+    /**
+     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     */
     public function error_reporting(int $level): int
     {
         return error_reporting($level);
     }
 
+    /**
+     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     */
     public function ignore_user_abort(): int
     {
         return ignore_user_abort();
     }
 
+    /**
+     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     */
     public function ini_set(string $key, string $value): bool
     {
         return false !== ini_set($key, $value);
     }
 
+    /**
+     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     */
     public function ini_get(string $key): ?string
     {
         $ini = ini_get($key);
