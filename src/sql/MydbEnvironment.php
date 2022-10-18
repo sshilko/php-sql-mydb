dafbdf632a766940de95b459738320a844d9e8a9
Sergei Shilko <contact@sshilko.com>
2022-10-19 00:59:09 +0200

    signal handlers

--- src/sql/MydbEnvironment.php
+++ src/sql/MydbEnvironment.php
@@ -14,15 +14,23 @@ declare(strict_types = 1);
 
 namespace sql;
 
+use sql\MydbException\EnvironmentException;
+use function count;
 use function error_reporting;
 use function gc_collect_cycles;
 use function gc_enabled;
 use function ignore_user_abort;
-use function ini_get;
 use function ini_set;
+use function pcntl_signal;
+use function pcntl_signal_dispatch;
+use function pcntl_signal_get_handler;
 use function set_error_handler;
 use const E_ALL;
 use const E_STRICT;
+use const SIG_DFL;
+use const SIGHUP;
+use const SIGINT;
+use const SIGTERM;
 
 /**
  * @author Sergei Shilko <contact@sshilko.com>
@@ -32,6 +40,20 @@ use const E_STRICT;
  */
 class MydbEnvironment
 {
+    protected array $knownSignals = [SIGTERM, SIGINT, SIGHUP];
+
+    /**
+     * @var array<int>
+     */
+    protected array $trappedSignals = [];
+
+    /**
+     * Backup of signal handlers
+     *
+     * @var array
+     */
+    protected array $trappedHandlers = [];
+
     /**
      * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
      */
@@ -52,9 +74,12 @@ class MydbEnvironment
         return set_error_handler($callback ?? $this->getNullErrorHandler(), $error_levels);
     }
 
+    /**
+     * @throws EnvironmentException
+     */
     public function setMysqlndNetReadTimeout(string $timeoutSeconds): bool
     {
-        return $this->ini_set('mysqlnd.net_read_timeout', $timeoutSeconds);
+        return (bool) $this->ini_set('mysqlnd.net_read_timeout', $timeoutSeconds);
     }
 
     /**
@@ -75,23 +100,75 @@ class MydbEnvironment
 
     /**
      * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     * @throws EnvironmentException
      */
-    public function ini_set(string $key, string $value): bool
+    public function ini_set(string $key, string $value): string
     {
-        return false !== ini_set($key, $value);
+        $result = ini_set($key, $value);
+        if (false === $result) {
+            throw new EnvironmentException();
+        }
+
+        return $result;
     }
 
     /**
-     * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+     * Disable custom signal handler
+     *
+     * @see https://wiki.php.net/rfc/async_signals
+     * @see https://blog.pascal-martin.fr/post/php71-en-other-new-things/
+     * @return array|null array of trapped signals
+     * @throws EnvironmentException
      */
-    public function ini_get(string $key): ?string
+    public function endSignalsTrap(): ?array
     {
-        $ini = ini_get($key);
-        if (false === ini_get($key)) {
-            return null;
+        /**
+         * Process signals
+         */
+        if (!pcntl_signal_dispatch()) {
+            throw new EnvironmentException();
         }
 
-        return $ini;
+        $trappedSignals = $this->trappedSignals;
+        foreach ($this->knownSignals as $signalNumber) {
+            /**
+             * Reset signals to previous/default handler
+             */
+            if (!pcntl_signal($signalNumber, $this->trappedHandlers[$signalNumber] ?? SIG_DFL)) {
+                throw new EnvironmentException();
+            }
+            unset($this->trappedHandlers[$signalNumber]);
+        }
+        $this->trappedSignals = [];
+
+        return count($trappedSignals) > 0
+            ? $trappedSignals
+            : null;
+    }
+
+    /**
+     * Enable custom signal handler
+     *
+     * @see https://wiki.php.net/rfc/async_signals
+     * @see https://blog.pascal-martin.fr/post/php71-en-other-new-things/
+     * @throws EnvironmentException
+     */
+    public function startSignalsTrap(): void
+    {
+        $this->trappedSignals = [];
+
+        $signalHandler = function (int $signalNumber): void {
+            $this->trappedSignals[] = $signalNumber;
+        };
+
+        foreach ($this->knownSignals as $signalNumber) {
+            $originalNandler = pcntl_signal_get_handler($signalNumber);
+            $this->trappedHandlers[$signalNumber] = $originalNandler;
+
+            if (!pcntl_signal($signalNumber, $signalHandler)) {
+                throw new EnvironmentException();
+            }
+        }
     }
 
     protected function getNullErrorHandler(): callable
